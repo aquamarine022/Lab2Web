06efc2144c56ecf3719892f00b3312df5695c594
mary <brelmari3000@gmail.com>
2024-11-03 20:17:45 +0300

    sth done

---
+++ src/main/java/com/server/Data.java
@@ -0,0 +1,26 @@
+package com.server;
+
+import jakarta.ejb.Stateful;
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.inject.Named;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Stateful
+@SessionScoped
+@Named("data")
+public class Data implements Serializable {
+    private final List<PointBean> data = new ArrayList<>();
+
+    public Data(){}
+
+    public List<PointBean> getData() {
+        return data;
+    }
+
+    public void addRPoint(PointBean point) {
+        data.add(point);
+    }
+}
---
+++ src/main/java/com/server/PointBean.java
@@ -0,0 +1,63 @@
+package com.server;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+public class PointBean implements Serializable {
+    private double x;
+    private double y;
+    private double r;
+    private boolean hit;
+    private LocalDateTime time;
+    private long calculationTime;
+
+    public PointBean() {}
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getR() {
+        return r;
+    }
+
+    public void setR(double r) {
+        this.r = r;
+    }
+
+    public boolean isHit() {
+        return hit;
+    }
+
+    public void setHit(boolean hit) {
+        this.hit = hit;
+    }
+
+    public LocalDateTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalDateTime time) {
+        this.time = time;
+    }
+
+    public long getCalculationTime() {
+        return calculationTime;
+    }
+
+    public void setCalculationTime(long calculationTime) {
+        this.calculationTime = calculationTime;
+    }
+}
